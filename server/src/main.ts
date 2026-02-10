177e3b26e5d7982c15c38569e45cee6865e863a2
JayveeFortissimo <jayveemaniquiz0123@gmail.com>
2026-02-10 17:49:12 +0800

    chore: fix CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,12 +7,27 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors(
-    {
-        credentials: true,
-        origin: ["http://localhost:5173", "https://try-red-eta.vercel.app"]
-    }
-));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://try-red-eta.vercel.app",
+  "https://try-r22pmp68y-cristobals.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use('/api', route);
 
